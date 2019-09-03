86baca3293011491ace8ce80165a9be619e0d593
bindubogarapu <bogarapu.bindu@gmail.com>
2019-09-03 16:20:44 +0530

    last tranactions test cases

--- src/main/java/com/hcl/mortgage/service/UserServiceImpl.java
+++ src/main/java/com/hcl/mortgage/service/UserServiceImpl.java
@@ -2,7 +2,6 @@ package com.hcl.mortgage.service;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.Base64;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -14,7 +13,6 @@ import com.hcl.mortgage.dto.LoginDetailsDto;
 import com.hcl.mortgage.dto.LoginDto;
 import com.hcl.mortgage.entity.Customer;
 import com.hcl.mortgage.repository.CustomerRepository;
-import com.hcl.mortgage.util.MortgageConstants;
 
 /**
  * @author Lakshmi
@@ -44,13 +42,13 @@ public class UserServiceImpl implements UserService {
 		if (customer.isEmpty()) {
 			loginResponseDto = new LoginDetailsDto();
 			loginResponseDto.setStatusCode(401);
-			loginResponseDto.setMessage(MortgageConstants.LOGIN_FAILURE);
+			loginResponseDto.setMessage("incorrect credintilas");
 		} else {
 			Customer customers = customer.get(0);
 			loginResponseDto = new LoginDetailsDto();
 			loginResponseDto.setCustomerId(customers.getCustomerId());
 			loginResponseDto.setStatusCode(200);
-			loginResponseDto.setMessage(MortgageConstants.LOGIN_SUCCESS);
+			loginResponseDto.setMessage("login succsessfully");
 
 		}
 		return loginResponseDto;
