95f33e763849d791772561765a0cbc367630c6ee
bindubogarapu <bogarapu.bindu@gmail.com>
2019-09-03 14:46:46 +0530

    mai api added

---
+++ src/main/java/com/hcl/mortgage/service/EmailSender.java
@@ -0,0 +1,44 @@
+package com.hcl.mortgage.service;
+
+import javax.mail.internet.MimeMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailSender {
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(EmailSender.class);
+	@Autowired
+	JavaMailSender mailSender;
+
+	public String sendOtp(String mailId, String headMessage, String bodyMessage) {
+
+		LOGGER.info("Enter send mail");
+
+		String returnString = "Email sent sucess";
+		try {
+
+			MimeMessage message = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message);
+
+			helper.setTo(mailId);
+			helper.setSubject(headMessage);
+			helper.setText(bodyMessage);
+
+			mailSender.send(message);
+
+		} catch (Exception e) {
+			returnString = "Mail failed";
+			LOGGER.error(e.getMessage());
+		}
+		return returnString;
+
+	}
+	
+	
+}
